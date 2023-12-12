a6107b45a7fc012eccd4f829872b73c9f9de7bf6
Elliot Mitchell <elliot.r.mitchell@gmail.com>
2023-12-11 22:32:39 -0500

    Day 6 pt 2

--- app/src/test/java/com/erm/adventofcode/twenty_three/Day6.kt
+++ app/src/test/java/com/erm/adventofcode/twenty_three/Day6.kt
@@ -54,4 +54,44 @@ class Day6 {
         println("aggregated ways =$aggregatedPoints")
         assert(aggregatedPoints == 288L)
     }
+    @Test
+    fun day6_p2() {
+        var aggregatedPoints = 1L
+        var times = mutableListOf<Long>()
+        var distances = mutableListOf<Long>()
+        Util.readLinesFromTextInput("day6input.txt") {
+            if (it.contains("Time")) {
+                val oneTime = it.split(":").toMutableList()
+                    .apply {
+                        removeAt(0)
+                    }[0]
+                    .split(" ")
+                    .mapNotNull { it.ifBlank { null }?.trim()}
+                times.add(oneTime.fold("0") {
+                    acc, s -> acc + s
+                }.toLong())
+
+            } else if (it.contains("Distance")) {
+                val onDistance = it.split(":").toMutableList()
+                    .apply {
+                        removeAt(0)
+                    }[0]
+                    .split(" ")
+                    .mapNotNull { it.ifBlank { null }?.trim() }
+                distances.add(onDistance.fold("0") {
+                        acc, s -> acc + s
+                }.toLong())
+            }
+            true
+        }
+        times.forEachIndexed { index, time ->
+            val race = Race(time, distances[index])
+
+            aggregatedPoints *= race.waysToWin()
+        }
+
+
+        println("aggregated ways =$aggregatedPoints")
+        assert(aggregatedPoints == 71503L)
+    }
 }
